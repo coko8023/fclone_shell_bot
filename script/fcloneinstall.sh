f5e943d64000b2fee5c0e71b843ca7aa858f1495
coko8023 <59942197+coko8023@users.noreply.github.com>
2020-08-17 01:18:28 +0800

    Update fcloneinstall.sh

--- script/fcloneinstall.sh
+++ script/fcloneinstall.sh
@@ -2,17 +2,13 @@
 rm -rf fclone.sh
 wget https://raw.githubusercontent.com/cgkings/fclone_shell_bot/master/script/fclone.sh
 echo "【fclone一键转存脚本自用】脚本配置"
-read -p "输入配置fclone的名称:" fcloneid
-sed -i "s/goog/$fcloneid/g" fclone.sh
-read -p "请输入0#中转盘ID（默认）:" tdid0
-sed -i "s/myid0/$tdid0/g" fclone.sh
-read -p "请输入1#ADV盘ID:" tdid1
-sed -i "s/myid1/$tdid1/g" fclone.sh
-read -p "请输入2#MDV盘ID:" tdid2
-sed -i "s/myid2/$tdid2/g" fclone.sh
-read -p "请输入3#BOOK盘ID:" tdid3
-sed -i "s/myid3/$tdid3/g" fclone.sh
+read -p "fcgd" fcloneid
+sed -i "s/goog/$fcgd/g" fclone.sh
+read -p "0AEJbZSxAPNh5Uk9PVA" tdid0
+sed -i "s/myid0/$0AEJbZSxAPNh5Uk9PVA/g" fclone.sh
+read -p "0AHy7KS-SS6LwUk9PVA" tdid1
+sed -i "s/myid1/$0AHy7KS-SS6LwUk9PVA/g" fclone.sh
 echo "如需增减目标地址，可自行修改fcloneinstall.sh和fclone.sh"
 mkdir -p ~/gclone_log/
 chmod +x fclone.sh
-echo "请输入 ./fclone.sh 使用脚本"
+echo "./fclone.sh"
